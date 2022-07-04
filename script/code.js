36a43d1c2d14efc69106be973b0ee8fc538587b8
DanielJamesF <fredericksd17@gmail.com>
2022-07-04 14:43:21 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,18 @@
+const form = Vue.createApp({
+    data() {
+        return {
+            name: '',
+            age: ''
+        }
+    },
+    methods: {
+        enterDetails() {
+        this.age>17 == age;
+        }
+    },
+    enterDetails() {
+        this.age<17 == !this.age
+    }
+})
+
+app.mount('#form')
